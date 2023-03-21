2f6d364b10da43d28860273515d4da718b7f6480
Lucas Cordiviola <lucarda27@hotmail.com>
2023-03-21 04:36:09 -0300

    script

--- linux-multi-arch.sh
+++ linux-multi-arch.sh
@@ -8,13 +8,12 @@ OUT="PDLIBDIR=./out"
 
 sudo apt update
 sudo apt install g++-arm-linux-gnueabihf g++-aarch64-linux-gnu -y
-sudo apt install build-essential binutils -y
-sudo apt install gcc-multilib -y
-make clean
 make CC=arm-linux-gnueabihf-gcc $SRC $OUT extension=l_arm install
 make clean
 make CC=aarch64-linux-gnu-gcc $SRC $OUT extension=l_arm64 install
 make clean
+sudo apt install build-essential binutils -y
+sudo apt install gcc-multilib -y
 make CFLAGS=-m32 LDFLAGS=-m32 $SRC $OUT extension=l_i386 install
 make clean
 make $SRC $OUT extension=l_amd64 install
